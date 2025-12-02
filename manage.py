3d2aa64165eac2e1b50dfba34ec36b99804e7dc2
Punya Swaroop Sirigiri <punyaswaroop12@users.noreply.github.com>
2025-12-02 04:17:57 -0500

    Create Django site with donation support

---
+++ manage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'temple_site.settings')
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
---
+++ temple_site/core/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+
+# Register your models here (if you add any later).
